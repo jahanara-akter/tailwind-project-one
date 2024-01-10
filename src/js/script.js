ec4c4e4bc3f2bda768f70d08b61b705cb146734b
Jahanara Akter <jahanaraaktersuma100@gmail.com>
2024-01-10 17:24:52 +0600

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector(".menu");
+let bar = document.querySelector(".bar");
+
+
+bar.addEventListener("click",function(){
+    menu.classList.toggle("show");
+});
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      Poppins: ['Poppins'],
+      Manrope: ['Manrope'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
